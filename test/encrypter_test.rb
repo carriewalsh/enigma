57afee476b69bf226666f46993aea6123fc299d1
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 15:56:27 -0700

    Adds attributes tests for Encrypter

--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -1,4 +1,23 @@
 require "./test/test_helper"
 
 class EncrypterTest < MiniTest::Test
+  def setup
+    @encrypter = Encrypter.new("butts","12345","032489")
+  end
+
+  def test_encrypter_exists
+    assert_instance_of Encrypter, @encrypter
+  end
+
+  def test_encrypter_has_message
+    assert_equal "butts", @encrypter.message
+  end
+
+  def test_encrypter_has_key
+    assert_equal "12345", @encrypter.key
+  end
+
+  def test_encrypter_has_offset
+    assert_equal "032489", @encrypter.offset
+  end
 end
