942a9009fb3b83da6eeb3921da56b18816537ecc
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 11:04:06 -0700

    Changes Keys to module

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,2 +1,2 @@
-class Keys
+module Keys
 end
--- test/keys_test.rb
+++ test/keys_test.rb
@@ -5,11 +5,7 @@ class KeysTest < MiniTest::Test
   def setup
     @keys = Keys.new
   end
-
-  def test_keys_exist
-    assert_instance_of Keys, @keys
-  end
-
+  
   def test_keys_default_with_empty_hash
     keys = Keys.new
       assert_equal ({}), keys.keys
