b589ba190c33188eda33d09e37482d69e75ec27b
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 12:04:10 -0700

    Adds create_keys test

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -11,29 +11,41 @@ class ShiftsTest < MiniTest::Test
   end
 
   def test_shifts_begins_with_skeleton_keys_hash
-    assert_equal ({a: nil,
-                    b: nil,
-                    c: nil,
-                    d: nil}), @shifts.keys
+    expected = {a: nil,
+                b: nil,
+                c: nil,
+                d: nil}
+    assert_equal expected, @shifts.keys
   end
 
   def test_shifts_begins_with_skeleton_offsets_hash
-    assert_equal ({a: nil,
-                    b: nil,
-                    c: nil,
-                    d: nil}), @shifts.offsets
+    expected = {a: nil,
+                b: nil,
+                c: nil,
+                d: nil}
+    assert_equal expected, @shifts.offsets
   end
 
   def test_shifts_begins_with_skeleton_shifts_hash
-    assert_equal ({a: nil,
-                    b: nil,
-                    c: nil,
-                    d: nil}), @shifts.shifts
+    expected = {a: nil,
+                b: nil,
+                c: nil,
+                d: nil}
+    assert_equal expected, @shifts.shifts
   end
 
   def test_random_five_digit_number_generated
     assert_equal String, @shifts.random_key.class
     assert_equal 5, @shifts.random_key.to_s.length
     assert_equal true, @shifts.random_key.to_i.between?(0,99999)
+  end
+
+  def test_keys_made_and_stored_in_hash
+    @shifts.create_keys(12345)
+    expected = {a: 12,
+                b: 23,
+                c: 34,
+                d: 45}
+    assert_equal expected, @shifts.keys
   end
 end
