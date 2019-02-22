1d807996b3e4f32176c386e26904bd5a96fa7858
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 08:02:53 -0700

    Adds KeysTest for default empty hash

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -9,4 +9,9 @@ class KeysTest < MiniTest::Test
   def test_keys_exist
     assert_instance_of Keys, @keys
   end
+
+  def test_keys_default_with_empty_hash
+    keys = Keys.new
+      assert_equal ({}), keys.keys
+  end
 end
