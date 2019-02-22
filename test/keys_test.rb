39db1be0d16fc2cac4369ab5fa49e0ca99842dfb
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 11:22:44 -0700

    Puts Shifts in KeysTest setup

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -2,12 +2,12 @@ require "./test/test_helper"
 
 class KeysTest < MiniTest::Test
 
-  # def setup
-  #   @keys = Keys.new
-  # end
-  #
-  # def test_keys_default_with_empty_hash
-  #   keys = Keys.new
-  #     assert_equal ({}), keys.keys
-  # end
+  def setup
+    @shifts = Shifts.new
+  end
+
+  def test_keys_default_with_empty_hash
+    keys = Keys.new
+      assert_equal ({}), keys.keys
+  end
 end
