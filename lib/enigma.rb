d666e1724e7d91d1a56f6aef9059b64a8f87ceaf
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 16:28:13 -0700

    Moves Encrypter init to Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,10 @@
 class Enigma
+  attr_reader :message,
+              :key,
+              :offset
+  def initialize(message: , key:, offset:)
+    @message = message
+    @key = key
+    @offset = offset
+  end
 end
--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -1,13 +1,6 @@
 require "./test/test_helper"
 
 class EncrypterTest < MiniTest::Test
-  def setup
-    @encrypter = Encrypter.new(message: "butts", key: "12345", offset: "032489")
-  end
 
-  def test_encrypter_exists
-    assert_instance_of Encrypter, @encrypter
-  end
 
-  
 end
