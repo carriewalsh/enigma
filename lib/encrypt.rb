ac07065ef0713be5e8571064bbda73252a5b1a76
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 12:06:32 -0700

    Adds modules to Encrypt class and corrects encrypt method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,5 +1,15 @@
+require "./lib/enigma"
+require "./lib/keys"
+require "./lib/offsets"
+require "pry"
+
 class Encrypt
-  #include undecided module(s)
+  include Keys
+  include Offsets
+
+enigma = Enigma.new
+key = enigma.shifts.random_key
+offset = enigma.shifts.today_offset
 
 reader = File.open(ARGV[0],"r")
 
@@ -7,10 +17,11 @@ message = reader.read
 reader.close
 
 writer = File.open(ARGV[1],"w")
-encrypted = message.encrypt
+encrypted = enigma.encrypt(message,key,offset)
 #should encrypt be here or in another module?
 
 writer.write(encrypted)
 writer.close
+puts "Created #{ARGV[1]} with the key #{key} and date #{offset}"
 
 end
