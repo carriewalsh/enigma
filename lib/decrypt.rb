e79c211904b34208a6bbe87c3de054d189426aa8
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 12:49:01 -0700

    Adds a little more to Decrypt class

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,12 +1,16 @@
+require "./lib/enigma"
+
 class Decrypt
   #include undecided module(s)
+
+  enigma = Enigma.new
 
   reader = File.open(ARGV[0],"r")
   encrypted = reader.read
   reader.close
 
   writer = File.open(ARGV[1],"w")
-  decrypted = encrypted.decrypt
+  decrypted = enigma.decrypt(encrypted,)
 
 
   writer.write(decrypted)
