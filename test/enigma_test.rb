0d1b3f3f1f263df866479b17a18237209332fe44
Carrie Walsh <carriewalsh@gmail.com>
2019-02-22 17:27:33 -0700

    Adds test for default alpha

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -27,4 +27,9 @@ class EnigmaTest < MiniTest::Test
     assert_nil enigma.key
     assert_nil enigma.offset
   end
+
+  def test_enigma_defaults_with_normal_alpha_array
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @enigma.alphabet
+  end
 end
