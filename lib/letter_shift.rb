75e53782cd7b2cb2c764b362ffb1fbc6fd8dad3d
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 19:13:29 -0700

    Moves alphabet if statement to own methods in LetterShift

--- lib/letter_shift.rb
+++ lib/letter_shift.rb
@@ -3,60 +3,68 @@ module LetterShift
     count = 0
     char_index(message).each_with_index do |char,index|
       if index == count
-        if char != nil
-          message[index] = @a_array[char]
-          count += 4
-        else
-          count += 4
-        end
+        a_alpha_if(message,char,index)
+        count += 4
       end
     end
     message #can I get rid of these returns and the tests since they are in the shift_all methods?
+  end
+
+  def a_alpha_if(message,char,index)
+    if char != nil
+      message[index] = @a_array[char]
+    end
   end
 
   def shift_b(message)
     count = 1
     char_index(message).each_with_index do |char,index|
       if index == count
-        if char != nil
-          message[index] = @b_array[char]
-          count += 4
-        else
-          count += 4
-        end
+        b_alpha_if(message,char,index)
+        count += 4
       end
     end
     message
+  end
+
+  def b_alpha_if(message,char,index)
+    if char != nil
+      message[index] = @b_array[char]
+    end
   end
 
   def shift_c(message)
     count = 2
     char_index(message).each_with_index do |char,index|
       if index == count
-        if char != nil
-          message[index] = @c_array[char]
-          count += 4
-        else
-          count += 4
-        end
+        c_alpha_if(message,char,index)
+        count += 4
       end
     end
     message
+  end
+
+  def c_alpha_if(message,char,index)
+    if char != nil
+      message[index] = @c_array[char]
+    end
   end
 
   def shift_d(message)
     count = 3
     char_index(message).each_with_index do |char,index|
       if index == count
-        if char != nil
-          message[index] = @d_array[char]
-          count += 4
-        else
-          count += 4
-        end
+        d_alpha_if(message,char,index)
+        count += 4
       end
     end
     message
+  end
+
+  def d_alpha_if(message,char,index)
+    if char != nil
+      message[index] = @d_array[char]
+    end
   end
 
   def shift_all(message)
