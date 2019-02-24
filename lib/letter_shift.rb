0faf7aed7ab286cee08eeaf103375ae5b6da310b
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 19:18:00 -0700

    Creates letter_back_alpha_if methods and shortens shift_back methods

--- lib/letter_shift.rb
+++ lib/letter_shift.rb
@@ -78,60 +78,68 @@ module LetterShift
     count = 0
     encryption.chars.each_with_index do |char, index|
       if index == count
-        if @alphabet.include?(char)
-          encryption[index] = @alphabet[@a_array.index(char)]
-          count += 4
-        else
-          count += 4
-        end
+        a_back_alpha_if(encryption,char,index)
+        count += 4
       end
     end
     encryption
+  end
+
+  def a_back_alpha_if(encryption,char,index)
+    if @alphabet.include?(char)
+      encryption[index] = @alphabet[@a_array.index(char)]
+    end
   end
 
   def shift_back_b(encryption)
     count = 1
     encryption.chars.each_with_index do |char, index|
       if index == count
-        if @alphabet.include?(char)
-          encryption[index] = @alphabet[@b_array.index(char)]
-          count += 4
-        else
-          count += 4
-        end
+        b_back_alpha_if(encryption,char,index)
+        count += 4
       end
     end
     encryption
+  end
+
+  def b_back_alpha_if(encryption,char,index)
+    if @alphabet.include?(char)
+      encryption[index] = @alphabet[@b_array.index(char)]
+    end
   end
 
   def shift_back_c(encryption)
     count = 2
     encryption.chars.each_with_index do |char, index|
       if index == count
-        if @alphabet.include?(char)
-          encryption[index] = @alphabet[@c_array.index(char)]
-          count += 4
-        else
-          count += 4
-        end
+        c_back_alpha_if(encryption,char,index)
+        count += 4
       end
     end
     encryption
+  end
+
+  def c_back_alpha_if(encryption,char,index)
+    if @alphabet.include?(char)
+      encryption[index] = @alphabet[@c_array.index(char)]
+    end
   end
 
   def shift_back_d(encryption)
     count = 3
     encryption.chars.each_with_index do |char, index|
       if index == count
-        if @alphabet.include?(char)
-          encryption[index] = @alphabet[@d_array.index(char)]
-          count += 4
-        else
-          count += 4
-        end
+        d_back_alpha_if(encryption,char,index)
+        count += 4
       end
     end
     encryption
+  end
+
+  def d_back_alpha_if(encryption,char,index)
+    if @alphabet.include?(char)
+      encryption[index] = @alphabet[@d_array.index(char)]
+    end
   end
 
   def shift_all_back(encryption)
