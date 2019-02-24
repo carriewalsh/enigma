e682890ca34356fdb0dcc39772368004bf2d95f7
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 20:53:31 -0700

    Adds initialized instances to Cracker class

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,2 +1,15 @@
 class Cracker
+  attr_reader :key,:offsets, :letter_align
+  def initialize
+    @key = nil
+    @offsets = {a: nil,
+                b: nil,
+                c: nil,
+                d: nil}
+
+    @letter_align = {a: nil,
+                b: nil,
+                c: nil,
+                d: nil}
+  end
 end
