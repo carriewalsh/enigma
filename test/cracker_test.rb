b70e2758f69d212090bfabe391253df6a48a9a55
Carrie Walsh <carriewalsh@gmail.com>
2019-02-23 20:50:03 -0700

    Creates Cracker/CrackerTest classes - early tests

--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -1,5 +1,16 @@
 require "./test/test_helper"
 
 class CrackerTest < MiniTest::Test
-  
+  def setup
+    @cracker = Cracker.new
+  end
+
+  def test_cracker_exists
+    assert_instance_of Cracker, @cracker
+  end
+
+  def test_cracker_starts_with_no_key
+    assert_nil @cracker.key
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,5 @@ require "./lib/shifts"
 require "./lib/enigma"
 require "./lib/letter_shift"
 require "./lib/letter_shift_back"
+require "./lib/cracker"
 require "./lib/encrypter" #delete later
