0ca5ca524f5ba7295da7d1d2bf5b78c497bec6e0
Carrie Walsh <carriewalsh@gmail.com>
2019-02-25 18:50:03 -0700

    Passes all tests for non-given key/offset in ecrypter

--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -6,7 +6,6 @@ class EncrypterTest < MiniTest::Test
   end
 
   def test_encrypt_hash_method_returns_hash
-    skip
     @enigma.shifts.create_keys(12345)
     @enigma.shifts.offset_integrated("032489")
     @enigma.shifts.create_shifts
@@ -14,11 +13,10 @@ class EncrypterTest < MiniTest::Test
     expected = {encryption: "message",
                 key: "12345",
                 date: "032489"}
-    assrt_equal expected, @enigma.encrypt_hash("message","12345","032489")
+    assert_equal expected, @enigma.encrypt_hash("message","12345","032489")
   end
 
   def test_encrypt_method_creates_encrypted_message
-    skip
     @enigma.shifts.create_keys(12345)
     @enigma.shifts.offset_integrated("032489")
     @enigma.shifts.create_shifts
@@ -26,11 +24,10 @@ class EncrypterTest < MiniTest::Test
     expected = {encryption: "urlci",
                 key: "12345",
                 date: "032489"}
-    assrt_equal expected, @enigma.encrypt("Ducks", "12345", "032489")
+    assert_equal expected, @enigma.encrypt("Ducks", "12345", "032489")
   end
 
   def test_encrypt_method_creates_encrypted_message_with_capitals
-    skip
     @enigma.shifts.create_keys(12345)
     @enigma.shifts.offset_integrated("032489")
     @enigma.shifts.create_shifts
@@ -38,11 +35,10 @@ class EncrypterTest < MiniTest::Test
     expected = {encryption: "urlci",
                 key: "12345",
                 date: "032489"}
-    assrt_equal expected, @enigma.encrypt("DUCKS", "12345", "032489")
+    assert_equal expected, @enigma.encrypt("DUCKS", "12345", "032489")
   end
 
   def test_encrypt_method_ignores_numbers_or_punctuation
-    skip
     @enigma.shifts.create_keys(12345)
     @enigma.shifts.offset_integrated("032489")
     @enigma.shifts.create_shifts
@@ -58,17 +54,14 @@ class EncrypterTest < MiniTest::Test
   end
 
   def test_if_no_key_given_uses_random_number
-    skip
     @enigma.encrypt("ducks2","032489")
-    assert_equal String,@enigma.shifts.keys.class
-    assert_equal 5,@enigma.shifts.keys.length
+    assert_equal Integer,@enigma.shifts.keys[:a].class
   end
 
   def test_if_no_date_given_uses_date_today
-    skip
     @enigma.encrypt("ducks2", "12345")
-    assert_equal String,@enigma.shifts.offsets.class
-    assert_equal 6,@enigma.shifts.offsets.length
+    assert_equal Integer,@enigma.shifts.offsets[:a].class
+    assert_equal 1,@enigma.shifts.offsets[:a].to_s.length
   end
 
   def test_if_no_date_or_offset_uses_random_and_today
