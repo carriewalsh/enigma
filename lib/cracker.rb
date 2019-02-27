f7aec7cf5f8a802447a65160f37c7e383ba9fa41
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 18:40:15 -0700

    Moves some methods from Cracker to Enigma

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -7,22 +7,6 @@ module Cracker
   include Wrongs
   include Decrypter
 
-  def count_chars(message)
-    message.length
-  end
-
-  def letter_pairs(message)
-    [" ","e","n","d"].zip(message[-4,4].chars)
-  end
-
-  def find_letter_align(message)
-    pairs = letter_pairs(message).rotate(-(message.length % 4))
-    @letter_align[:a] = pairs[0]
-    @letter_align[:b] = pairs[1]
-    @letter_align[:c] = pairs[2]
-    @letter_align[:d] = pairs[3]
-  end
-
   def calculate_shifts
     @letter_align.each do |letter,letter_pair|
       diff = @alphabet.index(letter_pair[1]) - @alphabet.index(letter_pair[0])
@@ -42,20 +26,6 @@ module Cracker
     @cipher.keys.each do |letter,key|
       @cipher.keys[letter] = key.to_s.rjust(2,"0")
     end
-  end
-
-  def options_array(key,ordinal)
-    @key_options_hash[ordinal] << key.to_s
-    @key_options_hash[ordinal] << (key.to_i + 27).to_s
-    @key_options_hash[ordinal] << (key.to_i + 54).to_s
-    @key_options_hash[ordinal] << (key.to_i + 81).to_s
-  end
-
-  def all_option_arrays
-    options_array(@cipher.keys[:a],:first)
-    options_array(@cipher.keys[:b],:second)
-    options_array(@cipher.keys[:c],:third)
-    options_array(@cipher.keys[:d],:fourth)
   end
 
   def check_digit(direction, number, array)
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -23,4 +23,34 @@ class Enigma
     @letter_align = {a: nil, b: nil, c: nil, d: nil}
     @key_options_hash = {first:[], second:[], third:[], fourth:[]}
   end
+
+  def options_array(key,ordinal)
+    @key_options_hash[ordinal] << key.to_s
+    @key_options_hash[ordinal] << (key.to_i + 27).to_s
+    @key_options_hash[ordinal] << (key.to_i + 54).to_s
+    @key_options_hash[ordinal] << (key.to_i + 81).to_s
+  end
+
+  def all_option_arrays
+    options_array(@cipher.keys[:a],:first)
+    options_array(@cipher.keys[:b],:second)
+    options_array(@cipher.keys[:c],:third)
+    options_array(@cipher.keys[:d],:fourth)
+  end
+
+  def count_chars(message)
+    message.length
+  end
+
+  def letter_pairs(message)
+    [" ","e","n","d"].zip(message[-4,4].chars)
+  end
+
+  def find_letter_align(message)
+    pairs = letter_pairs(message).rotate(-(message.length % 4))
+    @letter_align[:a] = pairs[0]
+    @letter_align[:b] = pairs[1]
+    @letter_align[:c] = pairs[2]
+    @letter_align[:d] = pairs[3]
+  end
 end
